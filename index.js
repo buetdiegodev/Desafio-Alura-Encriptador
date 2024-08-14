563118d026ecb68606a7a9b699d4e80cbc42bf88
buetdiegodev <buetdiegodev@gmail.com>
2024-08-13 23:59:55 -0300

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
 La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat"
+La letra "u" es convertsida para "ufat"
 */
 
 function mostrarTextarea() {
